95d27571b5a41a0f09e62e411d2ac41eca0ff561
Kalle <15094562+ThatKalle@users.noreply.github.com>
2025-02-25 13:27:45 +0000

    add devcontainer

---
+++ .devcontainer/postStartCommand.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# git submodule update --init || echo "no submodules to update"
+
+cd terraform/
+tflint --init
+cd ..
+
+# cd web/
+# nohup hugo server --baseURL localhost --poll 500ms & dockerize -wait tcp://localhost:1313 -timeout 30s
