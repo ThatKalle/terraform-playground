d590dda310ef947eec823889ebcb6d88679e090b
Kalle <15094562+ThatKalle@users.noreply.github.com>
2025-02-26 11:04:59 +0000

    move imports

---
+++ terraform/imports.tf
@@ -0,0 +1,14 @@
+import {
+  to = github_repository.this
+  id = "terraform-playground"
+}
+
+import {
+  to = github_branch.main
+  id = "terraform-playground:main"
+}
+
+import {
+  to = github_repository_environment.production
+  id = "terraform-playground:production"
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -10,18 +10,3 @@ terraform {
 }
 
 provider "github" {}
-
-import {
-  to = github_repository.this
-  id = "terraform-playground"
-}
-
-import {
-  to = github_branch.main
-  id = "terraform-playground:main"
-}
-
-import {
-  to = github_repository_environment.production
-  id = "terraform-playground:production"
-}
