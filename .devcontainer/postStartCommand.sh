83070992a5d745fb7260b1036a9516c656902dd1
Kalle <15094562+ThatKalle@users.noreply.github.com>
2025-03-17 12:04:42 +0000

    add version outputs

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-git submodule update --init || echo "no submodules to update"
+git submodule update --init
 
 cd terraform/
 tflint --init
+
+terraform --version
+tflint --version
+trivy --version
+tfprovidercheck --version
 cd ..
 
 cd web/
+dockerize --version
+hugo version
+
+if [ -f "nohup.out" ]; then rm nohup.out; fi
 nohup hugo server --baseURL localhost --poll 500ms & dockerize -wait tcp://localhost:1313 -timeout 30s
+cd ..
